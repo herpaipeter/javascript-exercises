2fcee90722155a966f14ef18992f60762182647e
Herpai Péter <herpaip@datapress.hu>
2022-04-30 08:48:44 +0200

    Add exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof(start) !== 'number' || typeof(end) !== 'number'
+        || start < 0 || end < 0) {
+        return 'ERROR'
+    }
+    let sum = 0;
+    let from = start < end ? start : end;
+    let to = start < end ? end : start;
+    for (let i = from; i <= to; ++i) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
