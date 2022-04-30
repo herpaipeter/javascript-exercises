875c59aac77fe579cc87aa6f5be80ccecccc8a81
Herpai Péter <herpaip@datapress.hu>
2022-04-30 08:48:38 +0200

    Add exercise 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let elem of args)
+    {
+        const ind = array.indexOf(elem);
+        if (-1 < ind) {
+            array.splice(ind, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
