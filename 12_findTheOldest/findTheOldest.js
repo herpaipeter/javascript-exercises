b78c9d915de6ed1f1597076cf7fd47d78adceb85
Herpai Péter <herpaip@datapress.hu>
2022-05-03 23:34:52 +0200

    Add exercise 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.reduce((oldest, person) => {
+        const age = (!person.yearOfDeath ? new Date().getFullYear() : person.yearOfDeath) - person.yearOfBirth;
+        return !oldest.age || oldest.age < age ? {person, age} : oldest;
+    }, {})
+    return oldest.person;
 };
 
 // Do not edit below this line
