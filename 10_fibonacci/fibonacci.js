0b877f2f25559da7560303d3bacf76c433586783
Herpai Péter <herpaip@datapress.hu>
2022-05-03 21:39:49 +0200

    Add exercise 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 1) {
+        return "OOPS";
+    }
+    let elements = [1, 1];
+     if (index < 3) {
+         return elements[index - 1];
+     }
+    for (let i = 2; i < index; ++i) {
+        elements.push(elements[i-2]+elements[i-1]);
+    }
+    return elements.pop();
 };
 
 // Do not edit below this line
