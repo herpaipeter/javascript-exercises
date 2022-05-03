88969d6d789ea55aabe06e40a47337982e9f50cb
Herpai Péter <herpaip@datapress.hu>
2022-05-03 21:32:56 +0200

    Add exercise 09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const forward = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g, '').toLowerCase();
+    const backward = forward.split('').reverse().join('');
+    return forward === backward;
 };
 
 // Do not edit below this line
